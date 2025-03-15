66983315f77eaab5aa63ce169cb486854c571c68
Juliana Aparecida Borges <107365569+JulianaProgramming@users.noreply.github.com>
2025-03-15 13:06:33 -0300

    Update envioDeMensagemNoWhatsApp.py

--- envioDeMensagemNoWhatsApp.py
+++ envioDeMensagemNoWhatsApp.py
@@ -1,30 +1,42 @@
 #Projeto enviar mensagem no whatsapp
-
 import pyautogui
 import time
 
 # pyautogui.write -> escrever um texto
 # pyautogui.press -> apertar 1 tecla
 # pyautogui.click -> clicar em algum lugar da tela
-# pyautogui.hotkey -> combinação de teclas
+# pyautogui.hotkey -> combinação de tecla
+
+# aguardando um tempinho para começar
 pyautogui.PAUSE = 0.5
 
-# abrir o aplicativo (WhatsApp)
+# abrindo o aplicativo (WhatsApp)
 pyautogui.press("win")
 pyautogui.write("whatsapp")
 pyautogui.press("enter")
-time.sleep(3)
-#pyautogui.click(x=411, y=254) #primeiro fixado no WhatsApp
-pyautogui.click(x=282, y=292) #segundo fixado no WhatsApp
+
+# aguardando alguns segundos para abrir o aplicativo
+time.sleep(7)
+
+# pesquisando o contato no WhatsApp
+pyautogui.hotkey("ctrl", "f")
+pyautogui.write("Minhas coisas")
+time.sleep(2)
+pyautogui.press("tab")
+time.sleep(2)
 pyautogui.press("enter")
 time.sleep(3)
 
+# clicando na barra para escrever a mensagem
+pyautogui.click(x=745, y=1001)
+time.sleep(3)
 
-# mandar mensagem 
+# inicializando o contador para mandar mensagem 
 number = 1
 
-while number <= 3: #enviará 3 mensagens seguidas
-    pyautogui.write("*Olá, tudo bem ?* ")
+# enviando as 3 mensagens seguidas
+while number <= 3: 
+    pyautogui.write("Oi, tudo bem ")
     pyautogui.press("enter")
     time.sleep(2)
     number = number + 1
